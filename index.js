c361ad331367ec5704ab816f65fa846c077e9958
Rafas2d2 <rafaelsoaressilva7@gmail.com>
2024-02-26 11:06:11 -0300

    primeiro comit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send('Hello Word!')
+})
+app.get('/Rafael', (req, res)=>{
+    res.send('Hello Rafael!')
+})
+app.get('/:nome', (req, res)=>{
+    res.send(req.params.nome)
+})
+app.get('/:nome/:sobrenome', (req, res)=>{
+    res.send(req.params.nome + "" + req.params.sobrenome)
+})
+app.get('/:soma/:valor/:valor2', (req, res)=>{
+    const soma = parseInt(req.params.valor) + parseInt(req.params.valor2)
+    res.send("O resultado da soma é: " + soma)
+})
+app.listen(port);
