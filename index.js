acc644d10eac6f575b86eaa696b744d1f4598527
Rafas2d2 <rafaelsoaressilva7@gmail.com>
2024-02-26 12:17:29 -0300

    Atualização no HTML

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res)=>{
-    res.send('Hello Word!')
+    res.render('index')
 })
 app.get('/Rafael', (req, res)=>{
     res.send('Hello Rafael!')
