4eba51bea210cdfc8c746323f559147cf834b88b
rafaelsoaressiva7@gmail.com <rafaelsoaressilva7@gmail.com>
2024-02-28 09:45:26 -0300

    Calculadora1.0

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
 
-app.get('/', (req, res)=>{
-    res.render('index')
+app.get('/calculo', (req, res)=>{
+    res.render('calculadora')
 })
 app.get('/Rafael', (req, res)=>{
     res.send('Hello Rafael!')
@@ -19,5 +20,14 @@ app.get('/:nome/:sobrenome', (req, res)=>{
 app.get('/:soma/:valor/:valor2', (req, res)=>{
     const soma = parseInt(req.params.valor) + parseInt(req.params.valor2)
     res.send("O resultado da soma é: " + soma)
+})
+app.post('/pesquisar', (req, res)=> {
+    res.send('Dados Recebidos: ' + req.body.nome)
+})
+app.post('/calculadora', (req, res)=>{
+    const v1 = parseInt(req.body.valor1) 
+    const v2 = parseInt(req.body.valor2)
+    const soma = v1 + v2
+    res.send('A soma é: ' + soma)
 })
 app.listen(port);
