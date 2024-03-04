df165541c1c4513f68f2f5233919b935905783f3
rafaelsoaressiva7@gmail.com <rafaelsoaressilva7@gmail.com>
2024-03-04 12:04:49 -0300

    CalculadoraCompleta

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const port = 3000
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}))
 
-app.get('/calculo', (req, res)=>{
-    res.render('calculadora')
-})
 app.get('/Rafael', (req, res)=>{
     res.send('Hello Rafael!')
+})
+app.get('/calculadora', (req,res)=>{
+    res.render('calculadora')
+})
+
+app.post('/calculadora', (req,res)=>{
+    const operacao = req.body.operacao;
+    const valor1 = parseInt(req.body.valor1)
+    const valor2 = parseInt(req.body.valor2)
+    let resultado;
+    switch(operacao){
+        case 'soma': resultado = valor1 + valor2; 
+        break;
+        case 'subtracao' : resultado = valor1 - valor2;
+         break;
+        case 'divisao' : resultado = valor1 / valor2; 
+        break;
+        case 'multiplicacao' : resultado = valor1 * valor2; 
+        break;
+    }
+    res.render('calculadora', {operacao:operacao, resultado:resultado})
+    
 })
 app.get('/:nome', (req, res)=>{
     res.send(req.params.nome)
@@ -23,11 +42,5 @@ app.get('/:soma/:valor/:valor2', (req, res)=>{
 })
 app.post('/pesquisar', (req, res)=> {
     res.send('Dados Recebidos: ' + req.body.nome)
-})
-app.post('/calculadora', (req, res)=>{
-    const v1 = parseInt(req.body.valor1) 
-    const v2 = parseInt(req.body.valor2)
-    const soma = v1 + v2
-    res.send('A soma é: ' + soma)
 })
 app.listen(port);
